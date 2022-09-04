db2a94580be6f334e1326b744827109e8e22ca8c
MonkeySaint <kyle.vardy1@gmail.com>
2022-09-04 01:52:58 -0400

    Works

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -1,12 +1,9 @@
 package com.example.addon;
 
 import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -19,16 +16,9 @@ public class Addon extends MeteorAddon {
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
-
-        // Modules
-        Modules.get().add(new ModuleExample());
-
+        LOG.info("Initializing Meteor Friend All Addon ");
         // Commands
         Commands.get().add(new CommandExample());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
     }
 
     @Override
--- src/main/java/com/example/addon/commands/CommandExample.java
+++ src/main/java/com/example/addon/commands/CommandExample.java
@@ -1,21 +1,36 @@
 package com.example.addon.commands;
 
+import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.systems.commands.Command;
+import meteordevelopment.meteorclient.systems.friends.Friend;
+import meteordevelopment.meteorclient.systems.friends.Friends;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import net.minecraft.command.CommandSource;
+import net.minecraft.text.Text;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
 public class CommandExample extends Command {
     public CommandExample() {
-        super("example", "Sends a message.");
+        super("friendall", "friend all online players.");
     }
 
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
-        builder.executes(context -> {
-            info("hi");
+        builder.executes(ctx -> {
+            execute();
             return SINGLE_SUCCESS;
         });
+    }
+
+    private void execute() {
+        assert mc.player != null;
+        ChatUtils.sendMsg(Text.of("Adding all friends, will lag."));
+        mc.player.networkHandler.getPlayerList().forEach(player -> {
+            GameProfile profile = player.getProfile();
+            Friends.get().add(new Friend(profile.getName(), profile.getId()));
+        });
+        ChatUtils.sendMsg(Text.of("Added all online players as a friend."));
     }
 }
